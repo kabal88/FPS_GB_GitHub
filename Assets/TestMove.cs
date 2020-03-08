0420a52614249d309f814cedfd540fb2470d06f4
Alexander Kabalkin <askabalkin@gmail.com>
2020-03-08 13:00:28 +0300

    Add Sources

---
+++ Assets/TestMove.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+
+namespace DefaultNamespace
+{
+    public sealed class TestMove : MonoBehaviour 
+    {
+        public UnityEngine.AI.NavMeshAgent agent { get; private set; }
+        public Transform target;                                  
+
+
+        private void Start()
+        {
+            agent = GetComponentInChildren<UnityEngine.AI.NavMeshAgent>();
+            
+        }
+
+
+        private void Update()
+        {
+            if (target != null)
+                agent.SetDestination(target.position);
+        }
+    }
+}
