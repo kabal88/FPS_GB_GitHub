98fde35db9f1ba65384ec5313d6d3e22687e66bc
Alexander Kabalkin <askabalkin@gmail.com>
2020-03-22 11:16:46 +0300

    Add Roads

---
+++ Assets/SpawnTester.cs
@@ -0,0 +1,38 @@
+﻿using UnityEngine;
+
+public class SpawnTester : MonoBehaviour
+{
+    #region PrivateData
+
+    #endregion
+
+
+    #region Fields
+
+    public float Max;
+    public float Min;
+
+    #endregion
+
+
+    #region Properties
+
+    #endregion
+
+
+    #region UnityMethods
+
+    private void OnDrawGizmos()
+    {
+        Gizmos.color = Color.cyan;
+        Gizmos.DrawWireSphere(transform.position, Min);
+        Gizmos.DrawWireSphere(transform.position, Max);
+    }
+
+    #endregion
+
+
+    #region Methods
+
+    #endregion
+}
